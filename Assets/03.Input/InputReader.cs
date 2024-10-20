30d9691d4323a78f8bab8272209fdbf606b04fa3
김진호 <russiakim2008@gmail.com>
2024-10-20 23:16:16 +0900

    One Control 1.5/2
    itch.io game jam Project

--- Assets/03.Input/InputReader.cs
+++ Assets/03.Input/InputReader.cs
@@ -11,12 +11,11 @@ using static InputReader;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
 
-//[CreateAssetMenu(menuName = "SO/InputReader")]
-public class InputReader : ScriptableObject
+[CreateAssetMenu(menuName = "SO/InputReader")]
+public class InputReader : ScriptableObject,PlayerInput.IWActions
 
 {
     public event Action<Vector2> OnMove;                      
-    public event Action OnJumpPressed, OnJumpReleased;         
 
     private PlayerInput _playerInputAction;
 
@@ -27,8 +26,15 @@ public class InputReader : ScriptableObject
         if (_playerInputAction == null)
         {
             _playerInputAction = new PlayerInput();
-           // _playerInputAction.W.SetCallbacks(this); //�÷��̾� ��ǲ�� �߻��ϸ� �� �ν��Ͻ��� ����
+            _playerInputAction.W.SetCallbacks(this);
         }
-        _playerInputAction.W.Enable(); //Ȱ��ȭ
+        _playerInputAction.W.Enable(); 
+    }
+
+
+    public void OnW(InputAction.CallbackContext context)
+    {
+        inputVector = context.ReadValue<Vector2>();
+        OnMove?.Invoke(inputVector);
     }
 }
