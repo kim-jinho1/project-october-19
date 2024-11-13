8f4926f1851235fd427a20a6a2d97ab6ee7fb3fb
김진호 <russiakim2008@gmail.com>
2024-11-13 21:48:18 +0900

    Remove Scripts
    Remove IdleState, MoveState, Player, State

--- Assets/01.Scripts/MoveData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveData : MonoBehaviour
-{
-    internal Vector3 moveDir;
-    public float moveSpeed = 5f;
-}
